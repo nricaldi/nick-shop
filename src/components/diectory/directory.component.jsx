987ea145105e411edb4f10b4fb2aaddc71b7dca9
Nicolas Ricaldi <nicolasricaldi@Nicolass-MBP.fios-router.home>
2021-02-15 22:54:56 -0500

    added homepage

---
+++ src/components/diectory/directory.component.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: [
+                {
+                    title: 'hats',
+                    imageUrl: 'https://images.unsplash.com/photo-1523380744952-b7e00e6e2ffa?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=3150&q=80',
+                    id: 1
+                }, 
+                {
+                    title: 'jackets',
+                    imageUrl: 'https://images.unsplash.com/photo-1560290625-14944c4409eb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80',
+                    id: 2
+                }, 
+                {
+                    title: 'sneakers',
+                    imageUrl: 'https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2850&q=80',
+                    id: 3
+                }, 
+                {
+                    title: 'womens',
+                    imageUrl: 'https://images.unsplash.com/photo-1596451984287-7a274406cbca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2800&q=80',
+                    size: 'large',
+                    pos: 'bottom',
+                    id: 4
+                }, 
+                {
+                    title: 'mens',
+                    imageUrl: 'https://images.unsplash.com/photo-1496345875659-11f7dd282d1d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80',
+                    size: 'large',
+                    pos: 'bottom',
+                    id: 5
+                }, 
+            ]
+        }
+    }
+
+    render() {
+        return(
+            <div className="container">
+
+                <div className="directory-menu">
+                    {
+                        this.state.sections.map(({ title, imageUrl, size, id }) => (
+                            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                        ))
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Directory;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Directory from '../../components/diectory/directory.component';
+
+import './homepage.styles.scss';
+
+
+
+const HomePage = () => {
+    return(
+        <div className="homepage">
+            <header>
+                <div className="container">
+                   <h1 className="logo">RCLD</h1>
+
+                </div>
+
+                {/* <nav>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav> */}
+            </header>
+
+            <section className="hero">
+                <div className="container">
+                    <h1>High Fashion On A Low Budget</h1>
+                </div>
+            </section>
+            <Directory/>
+        </div>
+    )
+}
+
+export default HomePage;
